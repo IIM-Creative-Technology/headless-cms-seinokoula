c2ab7b43d1e858a6ed559eddbcf73e261613dcf6
seinokoula <alexandre.charles92@gmail.com>
2023-03-05 20:51:38 +0100

    :heavy_plus_sign: .env added and previous and next project on single page

--- headless-nuxt/pages/projects/[slug].vue
+++ headless-nuxt/pages/projects/[slug].vue
@@ -3,28 +3,48 @@
     <div class="card">
       <p> url single : {{ $route.params.slug }}</p>
       <br>
-      <img :src="project.image.url" :alt="project.image.alternativeText" />
+      <!-- <img :src="project.image.url" :alt="project.image.alternativeText" /> -->
+      <img :src="project?.image?.url" :alt="project?.image?.alternativeText" />
       <p>nom du projet : {{ project.name }}</p>
       <p>description du projet : {{ project.description }}</p>
       <p v-for="technology in project.technologies" :key="technology.id">techno utilisée : {{ technology.techno }}</p>
       <p v-for="type_of_projects in project.type_of_projects" :key="type_of_projects.id">type de projet : {{
         type_of_projects.sections }}</p>
       <p><a :href="project.link" target="_blank">Link to project</a></p>
+      <div>
+        <button @click="prevProject">Previous Project</button>
+        <button @click="nextProject">Next Project</button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const { findOne } = useStrapi()
+const { find, findOne } = useStrapi()
 const route = useRoute()
 const project = ref()
+const allProjects = ref([])
 
 onMounted(async () => {
+  const data = await find('projects', '_sort=createdAt:desc&_limit=-1')
+  allProjects.value = data.data
   project.value = await findOne(`projects?filters[slug]=${route.params.slug}&populate=deep`)
   project.value = project.value.data[0]
-  console.log(project.value)
 })
 
+const prevProject = () => {
+  const currentIndex = allProjects.value.findIndex(p => p.slug === project.value.slug)
+  if (currentIndex > 0) {
+    project.value = allProjects.value[currentIndex - 1]
+  }
+}
+
+const nextProject = () => {
+  const currentIndex = allProjects.value.findIndex(p => p.slug === project.value.slug)
+  if (currentIndex < allProjects.value.length - 1) {
+    project.value = allProjects.value[currentIndex + 1]
+  }
+}
 
 </script>
 
@@ -75,5 +95,20 @@ a {
 a:hover {
   color: #283593;
   transform: scale(1.05);
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 2rem;
+}
+
+button {
+  border: none;
+  background-color: #3f51b5;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 30px;
 }
 </style>
